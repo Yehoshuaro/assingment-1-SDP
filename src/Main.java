a07e0c5a3207eec78d91bca5b07758eb34c81076
Yerkhan <231132@astanait.edu.kz>
2024-09-29 23:50:36 +0500

    1 Assignment for SDP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import Task_1.EmailValidator;
+import Task_1.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shape rectangle = new Rectangle(5, 3);
+        Shape circle = new Circle(4);
+
+        System.out.println("Rectangle area: " + rectangle.area());
+        System.out.println("Circle area: " + circle.area());
+
+        Bird sparrow = new Sparrow();
+        ((Flyable) sparrow).fly();
+
+        Bird penguin = new Penguin();
+        penguin.eat();
+
+        Workable humanWorker = new HumanWorker();
+        Workable robotWorker = new RobotWorker();
+
+        humanWorker.work();
+        ((Eatable) humanWorker).eat();
+
+        robotWorker.work();
+
+
+        Switchable light = new Light();
+        Button button = new Button(light);
+
+        button.press();
+
+
+        User user = new User("Alice", "alice@example.com");
+        EmailValidator validator = new EmailValidator();
+
+        if (validator.isValid(user.getEmail())) {
+            System.out.println(user.getName() + "'s email is valid.");
+        } else {
+            System.out.println(user.getName() + "'s email is invalid.");
+        }
     }
 }
+
---
+++ src/Task_1/EmailValidator.java
@@ -0,0 +1,7 @@
+package Task_1;
+
+public class EmailValidator {
+    public boolean isValid(String email) {
+        return email.contains("@") && email.contains(".");
+    }
+}
